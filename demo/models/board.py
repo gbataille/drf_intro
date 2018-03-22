fed60c1bbe44495836f7349178d1a4a45a32aa65
Grégory Bataille <gregory.bataille@gmail.com>
2018-03-22 17:36:24 +0100

    Add to base model

--- demo/models/board.py
+++ demo/models/board.py
@@ -5,3 +5,6 @@ class Board(models.Model):
 
     name = models.CharField(max_length=255, null=False, blank=False)
     description = models.TextField(null=False, blank=True)
+
+    def __str__(self):
+        return "Board %s: %s" % (self.id, self.name)
--- demo/models/item.py
+++ demo/models/item.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 from demo.models.board import Board
@@ -9,3 +10,7 @@ class Item(models.Model):
     title = models.CharField(max_length=255, null=False, blank=False)
     description = models.TextField(null=False, blank=True)
     creation_date = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(get_user_model(), models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return "Item %s: %s" % (self.id, self.title)
